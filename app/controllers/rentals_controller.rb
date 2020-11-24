f56c5a748109a0c8a7a5bb7e0d44834337cb9131
caroline jasinski <jasinski_caroline@yahoo.fr>
2020-11-24 18:07:53 +0100

    integration validate refused dans index rentals

--- app/controllers/rentals_controller.rb
+++ app/controllers/rentals_controller.rb
@@ -1,14 +1,5 @@
 class RentalsController < ApplicationController
 
-  def show
-    @user = current_user
-    @rental = Rental.find(params[:id])
-    if @rental.user_id == @user.id || Device.find(@rental.device_id).user_id == @user.id
-    else
-      render rentals_path, notice: "You are not allowed to enter this"
-    end
-  end
-
   def new
     @device = Device.find(params[:device_id])
     @rental = Rental.new
@@ -23,18 +14,6 @@ class RentalsController < ApplicationController
     redirect_to devices_path
     # @rental.device_id = @device.id
     # state = pending
-  end
-
-  def validated
-    rental = Rental.find(params[:id])
-    rental.update(state: "validated")
-    redirect_to rental_path(rental)
-  end
-
-  def refused
-    rental = Rental.find(params[:id])
-    rental.update(state: "refused")
-    redirect_to rental_path(rental)
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -3,15 +3,15 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   namespace :account do
-    resources :rentals, only: :index
-    end
-
-  resources :devices do  #foodparty505/device/
-    resources :rentals do #foodparty505/rental/
+    resources :rentals, only: [:index, :show] do
      member do
         patch 'validated'
         patch 'refused'
       end
     end
+  end
+
+  resources :devices do  #foodparty505/device/
+    resources :rentals #foodparty505/rental/
   end
 end
