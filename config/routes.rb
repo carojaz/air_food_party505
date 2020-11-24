ca1b8c5ae1930aca6f0cf857667e95e1d9794894
Aphisit Chompum <chompum.aphisit@hotmail.fr>
2020-11-24 13:27:54 +0100

    state

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   resources :devices  #foodparty505/device/
-  resource :rentals   #foodparty505/rental/
-
+  resource :rentals do #foodparty505/rental/
+    member do
+      patch 'validated'
+      patch 'refused'
+    end
+  end
 end
