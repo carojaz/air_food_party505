f7c1ff0fac5183d610dd9d8b17f0c62ede1cb886
caroline jasinski <jasinski_caroline@yahoo.fr>
2020-11-25 11:12:12 +0100

    resolve prod

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -32,5 +32,5 @@ import { flatpickr } from "../plugins/flatpickr";
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  flatpickr();
+
 });
