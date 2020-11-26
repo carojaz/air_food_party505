21e4de2b0c9f238f3ffbd00cd2394d94ab8ab92b
caroline jasinski <jasinski_caroline@yahoo.fr>
2020-11-26 16:16:29 +0100

    map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -34,7 +34,7 @@ import { initMapbox } from '../plugins/init_mapbox';
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
+  initMapbox();
   flatpickrFction();
   showInput();
-  initMapbox();
 });
