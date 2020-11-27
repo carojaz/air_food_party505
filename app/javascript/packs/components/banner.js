b59b1578e24310801fe652a9ebdbb164ed595e0f
caroline jasinski <jasinski_caroline@yahoo.fr>
2020-11-27 07:54:56 +0100

    typed home page load just for banner

--- app/javascript/packs/components/banner.js
+++ app/javascript/packs/components/banner.js
@@ -1,11 +1,13 @@
 import Typed from 'typed.js';
 
   const loadDynamicBannerText = () => {
-    new Typed('#banner-typed-text', {
-      strings: ["Let's go and rent your favorite food party device :", "Raclette, Pierrade, Crêpe party or a Pop Corn machine !!!"],
-      typeSpeed: 60,
-      loop: true
-    });
+    const elmt = document.querySelector(".banner");
+    if (elmt) {
+      new Typed('#banner-typed-text', {
+        strings: ["Let's go and rent your favorite food party device :", "Raclette, Pierrade, Crêpe party or a Pop Corn machine !!!"],
+        typeSpeed: 60,
+        loop: true
+      });
+    }
   }
-
 export { loadDynamicBannerText };
